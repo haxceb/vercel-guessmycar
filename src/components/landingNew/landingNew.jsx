3e786c5c43c3c6c056abbe9eb8803a64ee0c162b
= <=>
2021-03-26 14:43:30 +0500

    New Landing

--- src/components/landingNew/landingNew.jsx
+++ src/components/landingNew/landingNew.jsx
@@ -6,11 +6,17 @@ const landingNew = (props) => {
     
     return (
     <div className="">
-        <div className="flex-container">  
-        <div className="centeredTitle">{props.title}</div>
-    <div className="centeredSlogan">Elegant and Chic Furniture for those who Value Comfort</div></div>
-  
+        
     <img className="img-fluid" src={img} width="100%"></img>
+    <div className="flex-container">  
+        <div className="centeredTitle " style={{float:"left !important"}}>NEW<br/><strong>STYLE 2018</strong></div>
+        <div className="centeredSlogan">Elegant and Chic Furniture for those who Value Comfort</div>
+        <div className="buttonShop">
+           <button className="btn btn-dark" style={{float:"left"}}>Shop Now</button>
+        </div>
+    </div>
+
+  
     </div>
     )};
     
