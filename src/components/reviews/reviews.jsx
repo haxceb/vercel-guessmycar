a95bd4afe93b507c13ed7e468d7664188cf0b132
= <=>
2021-03-26 14:43:59 +0500

    All changes

---
+++ src/components/reviews/reviews.jsx
@@ -0,0 +1,48 @@
+import React,{Component} from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const UseStyles = makeStyles({
+    root: {
+      maxWidth: 250,
+      margin:"1em",
+    },
+    media: {
+      height: 140,
+    },
+  });
+const reviews = (props) => {
+    const classes = UseStyles();
+    return(
+        
+          <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="/static/images/cards/contemplative-reptile.jpeg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+     
+      </CardActions>
+    </Card>
+            
+
+
+    )};
+export default reviews;
